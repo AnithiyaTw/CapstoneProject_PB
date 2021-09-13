9381faff1cb83cab7d5532f9dad49f907fa6b9d4
Anithiya Tata Widhiyanti <anithiya.t.w01@gmail.com>
2021-09-13 08:32:17 +0700

    Activity_Anithiya

---
+++ app/src/main/java/com/example/capstoneproject_pb/Activity_Anithiya.java
@@ -0,0 +1,14 @@
+package com.example.capstoneproject_pb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Anithiya extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anithiya);
+    }
+}
