421d7dc4acef5b84dab81fa0ac489a85d104bf4b
Anithiya Tata Widhiyanti <anithiya.t.w01@gmail.com>
2021-10-01 12:39:00 +0700

    Ini SplashScreen

--- app/src/main/java/com/example/capstoneproject_pb/SplashScreen.java
+++ app/src/main/java/com/example/capstoneproject_pb/SplashScreen.java
@@ -2,13 +2,36 @@ package com.example.capstoneproject_pb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SplashScreen extends AppCompatActivity {
 
+Thread timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        timer = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized (this){
+                        wait(4000);
+                    }
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
