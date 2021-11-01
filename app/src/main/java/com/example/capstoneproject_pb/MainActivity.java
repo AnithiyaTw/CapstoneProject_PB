36c7b3c4688a9a5e96962e49d825403f735de8b6
Anithiya Tata Widhiyanti <anithiya.t.w01@gmail.com>
2021-11-01 23:09:04 +0700

    Revisi activity main

--- app/src/main/java/com/example/capstoneproject_pb/MainActivity.java
+++ app/src/main/java/com/example/capstoneproject_pb/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.capstoneproject_pb;
 
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private String mMessage;
+    public static final String EXTRA_MESSAGE = "com.example.CapstoneProject_PB.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void AplikasiBy(View view) {
+        mMessage = getString(R.string.by);
+        displayToast(mMessage);
+    }
+
+    public void DaftarWisata(View view) {
+        Intent intent = new Intent(MainActivity.this, DaftarWisata.class);
+        startActivity(intent);
+      }
 }
 
