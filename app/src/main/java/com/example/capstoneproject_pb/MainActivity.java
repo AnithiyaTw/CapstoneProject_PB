16507804b3874b2f8ba3e137b82b151e92e2bca3
Anithiya Tata Widhiyanti <anithiya.t.w01@gmail.com>
2021-11-01 23:16:26 +0700

    Revisi activity main dan tambah toast

--- app/src/main/java/com/example/capstoneproject_pb/MainActivity.java
+++ app/src/main/java/com/example/capstoneproject_pb/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();
-    }
+            }
 
 
     public void AplikasiBy(View view) {
